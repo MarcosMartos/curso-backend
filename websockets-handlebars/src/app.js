eb20d9a2f52e921dc943e0410ca22d5ad010ec8c
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-30 10:59:25 -0300

    avance de entregable websocket-handlebars

---
+++ websockets-handlebars/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import { __dirname } from "./utils";
+import { Server } from "socket.io";
+import viewsRouter from "./routes/views.router.js";
+import { productsManager } from "./productsManager.js";
+
+// Levantar servidor
+const app = express();
+
+// Configurar express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.use("/", viewsRouter);
+const httpServer = app.listen(8080, () => {
+  console.log("Escuchando al puerto 8080");
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log(`Cliente conectado: ${socket.id}`);
+
+  socket.on("createProduct", async (product) => {
+    const newProduct = await productsManager.createProduct(product);
+    socket.emit("productCreated", newProduct);
+  });
+});
