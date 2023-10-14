3f640161d012b73527fc5450e701f228d4f1f5e1
Marcos Martos <marcosmartostuvi@gmail.com>
2023-10-13 22:08:42 -0300

    Primera Práctica Integradora

---
+++ primeraPI/src/db/configDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const URI =
+  "mongodb+srv://marcosmartos:Escandinava@cluster0.4uhpvlo.mongodb.net/ecommerce?retryWrites=true&w=majority";
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((error) => console.log(error));
