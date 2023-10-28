30a1e9b30dc1d2e62dc18c376127b91389ae6e47
Marcos Martos <marcosmartostuvi@gmail.com>
2023-10-28 16:57:09 -0300

    Cambios desafío clase 10

--- Clase10/src/config/configDB.js
+++ Clase10/src/config/configDB.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const URI =
   "mongodb+srv://marcosmartos:Escandinava@cluster0.4uhpvlo.mongodb.net/clase10?retryWrites=true&w=majority";
 
-mongoose
-  .connect(URI)
-  .then(() => console.log("DB is connected"))
-  .catch((err) => console.log(err));
+async function databaseConnection() {
+  mongoose
+    .connect(URI)
+    .then(() => console.log("DB is connected"))
+    .catch((err) => console.log(err));
+}
+
+export default databaseConnection;
