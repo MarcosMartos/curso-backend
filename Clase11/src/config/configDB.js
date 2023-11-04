b4bf0273edba04b011fe6aa7e87d6e3af57112f4
Marcos Martos <marcosmartostuvi@gmail.com>
2023-11-04 20:18:38 -0300

    primeros pasos desafio clase 11

---
+++ Clase11/src/config/configDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const URI =
+  "mongodb+srv://marcosmartos:Escandinava@cluster0.4uhpvlo.mongodb.net/clase11?retryWrites=true&w=majority";
+
+async function databaseConnection() {
+  mongoose
+    .connect(URI)
+    .then(() => console.log("DB is connected"))
+    .catch((err) => console.log(err));
+}
+
+export default databaseConnection;
